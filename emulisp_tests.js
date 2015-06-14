19d8e9b507955a862f6d527c43c3b682ac116bd1
Christophe Gragnic <christophegragnic@gmail.com>
2015-06-14 22:58:40 +0200

    Tests: load picolisp.l file (std lib).

--- emulisp_tests.js
+++ emulisp_tests.js
@@ -3,10 +3,15 @@ fs.readFile('emulisp_core.js', 'utf8', function (err_js, emulisp_core_src) {
   if (err_js) {
     return console.log(err);
   }
-  fs.readFile('emulisp_tests.l', 'utf8', function (err_l, emulisp_tests_src) {
-    if (err_l) {
-      return console.log(err_l);
+  fs.readFile('picolisp.l', 'utf8', function (err_lib, emulisp_lib_src) {
+    if (err_lib) {
+      return console.log(err_lib);
     }
-    eval(emulisp_core_src + 'console.log(EMULISP_CORE.eval(emulisp_tests_src));');
+    fs.readFile('emulisp_tests.l', 'utf8', function (err_tests, emulisp_tests_src) {
+      if (err_tests) {
+        return console.log(err_tests);
+      }
+      eval(emulisp_core_src + 'console.log(EMULISP_CORE.eval(emulisp_lib_src+emulisp_tests_src));');
+    });
   });
 });
