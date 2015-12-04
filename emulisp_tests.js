ba518ac13eb85c6b6818c06de1dcea8bd6793ba3
Christophe Gragnic <christophegragnic@gmail.com>
2015-12-04 22:38:12 +0100

    Fix tests.

--- emulisp_tests.js
+++ emulisp_tests.js
@@ -1,12 +1,8 @@
 fs = require('fs')
-fs.readFile('emulisp_core.js', 'utf8', function (err_js, emulisp_core_src) {
-  if (err_js) {
-    return console.log(err);
+EMULISP_CORE = require('./emulisp_core.js');
+fs.readFile('emulisp_tests.l', 'utf8', function (err_l, emulisp_tests_src) {
+  if (err_l) {
+    return console.log(err_l);
   }
-  fs.readFile('emulisp_tests.l', 'utf8', function (err_l, emulisp_tests_src) {
-    if (err_l) {
-      return console.log(err_l);
-    }
-    eval(emulisp_core_src + 'console.log(EMULISP_CORE.eval(emulisp_tests_src));');
-  });
+  eval('console.log(EMULISP_CORE.eval(emulisp_tests_src));');
 });
