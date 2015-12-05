98a257cc2e0f58fd18d0b1c20dfefd64ac6b8435
Christophe Gragnic <christophegragnic@gmail.com>
2015-12-05 06:35:13 +0100

    Do not use 'eval' in emulisp_tests.js.

--- emulisp_tests.js
+++ emulisp_tests.js
@@ -4,5 +4,5 @@ fs.readFile('emulisp_tests.l', 'utf8', function (err_l, emulisp_tests_src) {
   if (err_l) {
     return console.log(err_l);
   }
-  eval('console.log(EMULISP_CORE.eval(emulisp_tests_src));');
+  EMULISP_CORE.eval(emulisp_tests_src);
 });
